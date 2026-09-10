28eb7c31cae5c2b4f48aa844d9a405b5c6e67608
kerdx <69465056+kerdx@users.noreply.github.com>
2026-09-10 18:23:30 +0200

    Remove peer profile avatar setting

--- app/src/main/java/pm/antani/resentin/net/dto/ShowPeerProfilesDto.kt
+++
@@ -1,9 +0,0 @@
-package pm.antani.resentin.net.dto
-
-import kotlinx.serialization.Serializable
-
-/** Account-wide preference controlling server-side peer profile/avatar discovery. */
-@Serializable
-data class ShowPeerProfilesDto(
-    val showPeerProfiles: Boolean = false,
-)
